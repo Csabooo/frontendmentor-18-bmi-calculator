137d0bba9e3191e91da56465f0f49204a60d0f74
Müller Csaba <csaba.muller@hotmail.com>
2023-09-25 20:52:46 +0200

    second  mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Calculator from "./components/Calculator/Calculator";
+import MainTable from "./components/MainTable/MainTable";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header></Header>
       <Calculator></Calculator>
+      <MainTable></MainTable>
     </>
   )
 }
