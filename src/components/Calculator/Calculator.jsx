ac156e2ac3aca6403b6dc03f5df6ce6965e5ba4f
Müller Csaba <csaba.muller@hotmail.com>
2023-10-17 21:46:16 +0200

    metric bmi results in calculator was  built in

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -7,10 +7,30 @@ import ResultsInfo from './ResultsInfo/ResultsInfo'
 function Calculator() {
 
     const [defaultState, setState] = useState(true);
+    const [defaultBMI, setBMI] = useState();
+
+    const [defaultResult, setResult] = useState(true);
+
 
     const radioButtonHandler = () => {
         setState(prevState => !prevState)
     }
+
+
+    const getValuesMetric = (bmi) => {
+        if (bmi > 0) {
+            setBMI(bmi);
+            setResult(false);
+        }
+
+        else {
+            setResult(true);
+        }
+
+    }
+
+
+
 
     return (
         <>
@@ -22,14 +42,14 @@ function Calculator() {
                 <div className={classes.radioButton}>
                     <div className={classes.row}>
                         <label htmlFor="metric">
-                            <input onClick={radioButtonHandler} type="radio" name="radio" id="metric" checked={defaultState} />
+                            <input onClick={radioButtonHandler} type="radio" name="radio" id="metric" defaultChecked={defaultState} />
                             <span className='ball'></span>
                         </label>
                         <span >Metric</span>
                     </div>
                     <div className={classes.row}>
                         <label htmlFor="imperial">
-                            <input onClick={radioButtonHandler} type="radio" name="radio" id="imperial" checked={!defaultState} />
+                            <input onClick={radioButtonHandler} type="radio" name="radio" id="imperial" defaultChecked={!defaultState} />
                             <span className='ball'></span>
                         </label>
                         <span>Imperial</span>
@@ -38,17 +58,24 @@ function Calculator() {
                 </div>
 
 
-                {defaultState ? (<Metric></Metric>) : (<Imperial></Imperial>)}
+                {defaultState ? (<Metric bmiIndex={getValuesMetric}></Metric>) : (<Imperial></Imperial>)}
 
-                <div className={classes.result}>
+
+                {defaultResult && <div className={classes.result}>
                     <h3>Welcome!</h3>
                     <p>
                         Enter your height and weight and you'll see your BMI result here
                     </p>
-                </div>
+                </div>}
+
+                {!defaultResult && <div className={classes.result}>
+                    <p>Your BMI is...</p>
+                    <h1>{defaultBMI}</h1>
+                    <p>Your BMI suggests you’re a healthy weight. Your ideal weight is between 63.3kgs - 85.2kgs.</p>
+                </div>}
 
 
-            </div>
+            </div >
             <ResultsInfo></ResultsInfo>
         </>
     )
--- src/components/Calculator/Metric/Metric.jsx
+++ src/components/Calculator/Metric/Metric.jsx
@@ -1,22 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from "./Metric.module.css";
+import { useEffect } from 'react';
 
-function Metric() {
+function Metric(props) {
+
+    const [defaultHeightCM, setHeightCM] = useState(0);
+    const [defaultWeightKG, setWeightKG] = useState(0);
+    const [bmi, setBmi] = useState();
+
+    useEffect(() => {
+        calculateBMI();
+    }, [defaultHeightCM, defaultWeightKG]);
+
+
+    const enterHeight = (event) => {
+        const height = event.target.value;
+        setHeightCM(height);
+    }
+
+    const enterWeight = (event) => {
+        setWeightKG(event.target.value);
+    }
+
+    props.bmiIndex(bmi);
+
+
+
+
+    const calculateBMI = () => {
+        if (defaultHeightCM > 0 && defaultWeightKG > 0) {
+            const heightInMeters = defaultHeightCM / 100;
+
+            const calculatedBmi = defaultWeightKG / (heightInMeters * heightInMeters);
+            setBmi(calculatedBmi.toFixed(1)); // 2 tizedesjegy pontossággal
+        }
+        else if (defaultHeightCM == 0 || defaultWeightKG == 0) {
+            setBmi("");
+        }
+
+
+    }
+
     return (
         <div className={classes.input_table}>
 
             <label htmlFor="height">Height</label>
             <div className='flex flex-col relative'>
-                <input type='text' placeholder=''></input>
+                <input type='text' placeholder='' onChange={enterHeight} value={defaultHeightCM}></input>
                 <span className="">cm</span>
             </div>
 
 
             <label htmlFor="weight">Weight</label>
             <div className='flex flex-col relative'>
-                <input type='text' placeholder=''></input>
+                <input type='text' placeholder='' onChange={enterWeight} value={defaultWeightKG}></input>
                 <span className="">kg</span>
             </div>
+            {/*  <p>{bmi}</p> */}
         </div>
     )
 }
